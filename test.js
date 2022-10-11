4cbac43c5b1a13210de75a450242d99b380007be
Markvitim <markvitim@gmail.com>
2022-10-11 21:42:45 +0300

    add ./lib/commands.js

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+const {
+  clickElement,
+  assretNoElementValue,
+  assretElementValue,
+} = require("./lib/commands.js");
 let page;
 
 describe("test", () => {
@@ -9,68 +14,54 @@ describe("test", () => {
   afterEach(() => {
     page.close();
   });
-
-  test("one ticket booking today", async () => {
-    const data = await page.waitForSelector(".page-nav > a:nth-child(1)");
-    await data.click();
-    const time = await page.waitForSelector("a.movie-seances__time");
-    await time.click();
-    const chair = await page.waitForSelector(
-      ".buying-scheme__wrapper > div:nth-child(2) > span:nth-child(5)"
+  test.only("one ticket booking today", async () => {
+    await clickElement(page, ".page-nav > a:nth-child(1)");
+    await clickElement(page, "section > div:nth-child(3) > ul > li");
+    await clickElement(
+      page,
+      ".buying-scheme__wrapper > div:nth-child(1) > span:nth-child(2)"
     );
-    await chair.click();
-    const booking = await page.waitForSelector("button.acceptin-button");
-    await booking.click();
-    const codeConfirmation = await page.waitForSelector(
-      "button.acceptin-button"
+    await clickElement(page, "button.acceptin-button");
+    await clickElement(page, "button.acceptin-button");
+    await assretElementValue(
+      page,
+      "h2.ticket__check-title",
+      "Электронный билет"
     );
-    await codeConfirmation.click();
-    const element = await page.waitForSelector("h2.ticket__check-title");
-    const value = await element.evaluate((el) => el.textContent, element);
-    expect(value).toEqual("Электронный билет");
+    //const element = await page.waitForSelector("h2.ticket__check-title");
+    //const value = await element.evaluate((el) => el.textContent, element);
+    //expect(value).toEqual("Электронный билет");
   });
 
   test("booking another time two tickets", async () => {
-    const data = await page.waitForSelector("nav>a:nth-child(3)");
-    await data.click();
-    const time = await page.waitForSelector(
-      "section > div:nth-child(3) > ul > li"
+    await clickElement(page, "nav>a:nth-child(3)");
+    await clickElement(page, "section > div:nth-child(3) > ul > li");
+    await clickElement(
+      page,
+      ".buying-scheme__wrapper > div:nth-child(1) > span:nth-child(1)"
     );
-    await time.click();
-    const chair1 = await page.waitForSelector(
-      ".buying-scheme__wrapper > div:nth-child(2) > span:nth-child(1)"
+    await clickElement(
+      page,
+      ".buying-scheme__wrapper > div:nth-child(1) > span:nth-child(2)"
     );
-    await chair1.click();
-    const chair2 = await page.waitForSelector(
-      ".buying-scheme__wrapper > div:nth-child(2) > span:nth-child(2)"
-    );
-    await chair2.click();
-    const booking = await page.waitForSelector("button.acceptin-button");
-    await booking.click();
-    const codeConfirmation = await page.waitForSelector(
-      "button.acceptin-button"
-    );
-    await codeConfirmation.click();
+    await clickElement(page, "button.acceptin-button");
+    await clickElement(page, "button.acceptin-button");
     const element = await page.waitForSelector("h2.ticket__check-title");
     const value = await element.evaluate((el) => el.textContent, element);
     expect(value).toEqual("Электронный билет");
   });
 
-  test.only("booking occupied place", async () => {
-    const data = await page.waitForSelector(".page-nav > a:nth-child(1)");
-    await data.click();
-    const time = await page.waitForSelector(
-      "section > div:nth-child(3) > ul > li"
-    );
-    await time.click();
-    const chair = await page.waitForSelector(
+  test("booking occupied place", async () => {
+    await clickElement(page, ".page-nav > a:nth-child(1)");
+    await clickElement(page, "section > div:nth-child(3) > ul > li");
+    await clickElement(
+      page,
       ".buying-scheme__wrapper > div:nth-child(2) > span:nth-child(2)"
     );
-    await chair.click();
-    const booking = await page.waitForSelector("button.acceptin-button");
-    await booking.click();
-    const element = await page.waitForSelector("section");
-    const value = await element.evaluate((el) => el.textContent, element);
-    expect(value).toEqual(expect.not.stringContaining("Электронный билет"));
+    await clickElement(page, "button.acceptin-button");
+    await assretNoElementValue(page, "section", "Электронный билет");
+    //const element = await page.waitForSelector("section");
+    //const value = await element.evaluate((el) => el.textContent, element);
+    //expect(value).toEqual(expect.not.stringContaining("Электронный билет"));
   });
 });
